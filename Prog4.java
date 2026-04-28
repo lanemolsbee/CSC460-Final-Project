df7f61e159f1639bded18c088f655715336ce8a1
Pearl McNabb <pearlmcnabb@dhcp-10-134-177-174.uawifi.arizona.edu>
2026-04-28 08:19:59 -0700

    Added Prog4.java

---
+++ Prog4.java
@@ -0,0 +1,37 @@
+import java.sql.*;
+import java.io.*;
+import java.util.*;
+
+public class Prog4 {
+    public static void main(String[] args) {
+        // put JDBC reading stuff here
+    }
+
+    // functionality #1 - Pearl
+    public boolean manageAccount() {
+        return true;
+    }
+
+    // functionality #2 - Jordan
+
+    // functionality #3 - Pearl
+    public boolean manageWorkspace() {
+        return true;
+    }
+
+    // functionality #4 - Jordan
+
+    // functionality #5 - Pearl
+    public boolean managePrompt() {
+        return true;
+    }
+
+    // functionality #6 - Jordan
+
+    // functionality #7 - Pearl
+    public boolean billingOperations() {
+        return true;
+    }
+
+    // functionality #8 - Jordan
+}
